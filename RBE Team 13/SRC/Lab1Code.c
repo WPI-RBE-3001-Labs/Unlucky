5dbc80221c6b57e922dbfc1b085172d9e39441c0
sangu <sangu@Sanguineo>
2017-02-07 11:07:39 -0500

    Reformatted code

--- RBE Team 13/SRC/Lab1Code.c
+++ RBE Team 13/SRC/Lab1Code.c
@@ -5,11 +5,62 @@
  *      Author: sangu
  */
 #include "RBELib/RBELib.h"
-int Q3A()
-{
-	int timerCountVal = 9;
 
+volatile unsigned long timerCounter = 0;
+unsigned long time = 0;
+ISR(TIMER0_COMPA_vect) {
+	timerCounter++;
+	if(timerCounter >= 1800)
+	{
+		timerCounter = 0;
+		time++;
+	}
+}
 
-		return timerCountVal;
+void initCLK() {
+	TCCR0B = (1 << CS02) | (1 << CS00); //sets the timer0 prescaler to 1024
+	//every time timerCounter == 1800
+
+}
+
+float angle = 0;
+int value = 0;
+int higByte = 0;
+float count = 0;
+float mV = 0;
+
+void readADC() {
+	ADCSRA = ADCSRA | (1 << ADSC);
+	//while ((ADCSRA & (1 << ADSC)) > 0) {
+	value = ADCL;
+	higByte = ADCH;
+	count = value + (higByte << 8);
+	mV = (count * ((5.0 * 10 * 10 * 10) / 1023.0));
+	angle = (count * (270.0 / 1023.0));
+	printf("Time: %0.01f Angle: %0.001f Count: %0.1f mV: %0.1f \n\r", time, angle, count, mV);
+
+	//}
+}
+float frequency = 10; //10Hz default
+unsigned int dutyCycle = 50; //default duty cycle
+void readSwitches() {
+	if (PINB1 == 1) {
+		frequency = 1;
+	}
+	if (PINB3 == 1) {
+		frequency = 20;
+	}
+	if (PINB5 == 1) {
+		frequency = 100;
+	}
+}
+void setClock(int newFrequency) {
+
+}
+void Lab1Code() {
+	readSwitches();
+	readADC();
+	initCLK();
+
 }
 
--- RBE Team 13/SRC/main.c
+++ RBE Team 13/SRC/main.c
@@ -6,83 +6,18 @@
 #include "RBELib/RBELib.h"
 #include <stdlib.h>
 //character for receiving serial data
-char inchar;
-int lowADC = 0;
-int highADC=1023;
-float pot1ang = 0;
-unsigned long potangint;
-float mV=0;
 
-volatile unsigned long sysTime = 0;
-volatile unsigned long timerCounter;
-volatile unsigned long intTime;
-volatile double timerCountVal = 9;
-int test = 10;
-//Timer0 should be default 1843200Hz
-
-ISR(TIMER0_OVF_vect){
-	timerCounter++;
-	if(timerCounter>=timerCountVal){
-		timerCounter=0;
-		sysTime++;
-		intTime++;
-		DDRC = 0xFF;
-		test = PINC0;
+int main(void) {
+	initRBELib(); //Setup printf() and setServo()
+	debugUSARTInit(115200); //Initialize UART
+	initADC(7);
+	DDRB = 0x00; //enable PORTB for switches
+	sei();
+	while (1)
+	{
+		Lab1Code();
+		//printf("Hello");
 	}
+	return 0;
 }
-
-
-int main(void){
-sei();
-initRBELib(); //Setup printf() and setServo()
-debugUSARTInit(115200); //Initialize UART
-initADC(7);
-
-int value = 0;
-int higByte = 0;
-float count = 0;
-float time = 0;
-float frequency = 0;
-
-while (1){
-//ADC Reading
-ADCSRA = ADCSRA | (1<<ADSC);
-while((ADCSRA & (1<< ADSC)) >0){
-	value = ADCL;
-	higByte=ADCH;
-	count = value + (higByte<<8);
-	time = sysTime;
-	float millis = (count*((5.0*10*10*10)/1023.0));
-	float angle = (count*(270.0/1023.0));
-	printf("Time: %f Angle: %f Count: %f mV: %f \n\r", time, angle, count, millis);
-
-}
-		DDRB = 0x00;
-		if(PINB1 == 1)
-		{
-			//1hz Prescaler of 256
-			TCCR0B |= 1 << CS02;
-			timerCountVal = 7200;
-			frequency = 1;
-		}
-		if(PINB3 == 1)
-			{
-				//20hz Prescaler of 8
-			TCCR0B |= 1 << CS01;
-			timerCountVal = 11520;
-			frequency = 20;
-			}
-		if(PINB5 == 1)
-			{
-				//100hz no prescaler
-			TCCR0B |= 1 << CS00;
-				timerCountVal = 18432;
-				frequency = 100;
-			}
-printf(test);
-
-}
-return 0;
-}
-
 
