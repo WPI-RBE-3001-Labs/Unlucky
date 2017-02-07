83f28b48f2c21767abe04602deee7a75d3ed3849
sangu <sangu@Sanguineo>
2017-02-07 11:27:34 -0500

    Added test for timer

--- RBE Team 13/SRC/Lab1Code.c
+++ RBE Team 13/SRC/Lab1Code.c
@@ -10,14 +10,10 @@ volatile unsigned long timerCounter = 0;
 unsigned long time = 0;
 ISR(TIMER0_COMPA_vect) {
 	timerCounter++;
-	if(timerCounter >= 1800)
-	{
-		timerCounter = 0;
-		time++;
-	}
 }
 
 void initCLK() {
+	TCCR0A = (1<<WGM01)|(1<<COM0A0); //???
 	TCCR0B = (1 << CS02) | (1 << CS00); //sets the timer0 prescaler to 1024
 	//every time timerCounter == 1800
 
@@ -37,7 +33,7 @@ void readADC() {
 	count = value + (higByte << 8);
 	mV = (count * ((5.0 * 10 * 10 * 10) / 1023.0));
 	angle = (count * (270.0 / 1023.0));
-	printf("Time: %0.01f Angle: %0.001f Count: %0.1f mV: %0.1f \n\r", time, angle, count, mV);
+	printf("Time: %0.1f Angle: %0.1f Count: %0.1f mV: %0.1f \n\r", time, angle, count, mV);
 
 	//}
 }
@@ -58,9 +54,17 @@ void setClock(int newFrequency) {
 
 }
 void Lab1Code() {
-	readSwitches();
-	readADC();
 	initCLK();
+	readSwitches();
+	if(timerCounter > 7200)
+	{
+		//Every 4 seconds (should be)
+		//if this works ten this means that the timer works just not reading correctly
+		readADC();
+		timerCounter = 0;
+	}
+
+
 
 }
 
