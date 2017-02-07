10eaac23067961b586c8425acba93cb56a3c6084
sangu <sangu@Sanguineo>
2017-02-07 16:34:21 -0500

    Time work now! Updates ADC once a second

--- RBE Team 13/SRC/Lab1Code.c
+++ RBE Team 13/SRC/Lab1Code.c
@@ -6,15 +6,22 @@
  */
 #include "RBELib/RBELib.h"
 
+int time = 0;
 volatile unsigned long timerCounter = 0;
 ISR(TIMER0_COMPA_vect) {
 	timerCounter++;
+	if(timerCounter >= 18000)
+	{
+		time ++;
+		timerCounter = 0;
+	}
 }
 
 void initCLK() {
 	TCCR0A = (1<<WGM01)|(1<<COM0A0); //???
 	TCCR0B = (1 << CS02) | (1 << CS00); //sets the timer0 prescaler to 1024
 	//every time timerCounter == 1800?, 1843200/1024
+	TIMSK0 = 0x2; //OCIEA enable
 
 }
 
@@ -24,16 +31,16 @@ int higByte = 0;
 float count = 0;
 float mV = 0;
 
+
 void readADC() {
 	ADCSRA = ADCSRA | (1 << ADSC);
 	//while ((ADCSRA & (1 << ADSC)) > 0) {
 	value = ADCL;
 	higByte = ADCH;
 	count = value + (higByte << 8);
-	float time = timerCounter;
 	mV = (count * ((5.0 * 10 * 10 * 10) / 1023.0));
 	angle = (count * (270.0 / 1023.0));
-	printf("Time: %0.1f Angle: %0.1f Count: %0.1f mV: %0.1f \n\r", time, angle, count, mV);
+	printf("Time: %0.1f Angle: %0.1f Count: %0.1f mV: %0.1f \n\r", (float)time, angle, count, mV);
 
 	//}
 }
@@ -58,18 +65,18 @@ void setCLK() {
 // Should set frequency and dutycycle when changed
 	//divide clock general counter by frequncy?
 }
+int lastSeen = 0;
 void Lab1Code() {
 	initCLK();
 	readSwitches();
-	if(timerCounter > 7200)
+	if(time == lastSeen +1)
 	{
-		//Every 4 seconds (should be)
-		//if this works ten this means that the timer works just not reading correctly
+		//Updates once a second
 		readADC();
-		timerCounter = 0;
-		//PIND4 = 0;
+		//PIND4 = 0
+		lastSeen = time;
 	}
-	if(timerCounter > 3600)
+	if(time%2 == 0)
 	{
 		//PIND4 = 1; //hook up LED to PORT D to test output
 	}
