250707f8c904f062f6cba20bd7bfafe1d62b708b
sangu <sangu@Sanguineo>
2017-02-07 11:45:13 -0500

    builds correctly

--- RBE Team 13/SRC/Lab1Code.c
+++ RBE Team 13/SRC/Lab1Code.c
@@ -7,7 +7,6 @@
 #include "RBELib/RBELib.h"
 
 volatile unsigned long timerCounter = 0;
-unsigned long time = 0;
 ISR(TIMER0_COMPA_vect) {
 	timerCounter++;
 }
@@ -31,6 +30,7 @@ void readADC() {
 	value = ADCL;
 	higByte = ADCH;
 	count = value + (higByte << 8);
+	float time = timerCounter;
 	mV = (count * ((5.0 * 10 * 10 * 10) / 1023.0));
 	angle = (count * (270.0 / 1023.0));
 	printf("Time: %0.1f Angle: %0.1f Count: %0.1f mV: %0.1f \n\r", time, angle, count, mV);
@@ -54,7 +54,7 @@ void readSwitches() {
 		frequency = 10; //default
 	}
 }
-void setPWM() {
+void setCLK() {
 // Should set frequency and dutycycle when changed
 	//divide clock general counter by frequncy?
 }
@@ -67,11 +67,11 @@ void Lab1Code() {
 		//if this works ten this means that the timer works just not reading correctly
 		readADC();
 		timerCounter = 0;
-		PIND4 = 0;
+		//PIND4 = 0;
 	}
 	if(timerCounter > 3600)
 	{
-		PIND4 = 1; //hook up LED to PORT D to test output
+		//PIND4 = 1; //hook up LED to PORT D to test output
 	}
 
 
