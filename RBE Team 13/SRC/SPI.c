fef963b6bb822ef384540f9ba1b9ecce7c11ff8a
sangu <sangu@130.215.215.241>
2017-02-20 18:08:40 -0500

    Created comment in SPIinit

--- RBE Team 13/SRC/SPI.c
+++ RBE Team 13/SRC/SPI.c
@@ -13,6 +13,12 @@ void initSPI(void) {
 	SPI_SCK = OUTPUT;
 	SPI_MISO = INPUT;
 
+	DAC_SS_ddr = 1;
+
+	DAC_SS = 1;
+	DAC_SS = 0;  //toggle DAC
+	DAC_SS = 1;
+
 	//Bit 7: Disable interrupts
 	//Bit 6: Enable the SPI lines
 	//Bit 5: MSB transmitted first
@@ -20,13 +26,6 @@ void initSPI(void) {
 	//Bit 3: CLK low when idle
 	//Bit 2: Sample on leading edge of signal
 	//Bits 1 - 0: 128 CLK prescaler
-
-	DAC_SS_ddr = 1;
-
-	DAC_SS = 1;
-	DAC_SS = 0;  //toggle DAC
-	DAC_SS = 1;
-
 	SPCR = 0x53;
 
 	DDRDbits._P7 = OUTPUT;
